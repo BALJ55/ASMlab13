3ddf03f9fc70f29381a62602e9d90fafed48d795
balj55 <byronamh@gmail.com>
2017-05-16 11:55:21 -0600

    agregado archivo correcto

--- prog23b_QUEMU.s
+++ prog23b_QUEMU.s
@@ -1,70 +1,70 @@
 /*** Using LEN and STRIDE to sum vectors ***/
-	.extern printf	
 	.global	main
 	.func main
 main:
-
-	SUB SP, SP, #24	@ room for printf
-	
-	LDR R1, addr_value1	@ Get addr of values
-	LDR R2, addr_value2
-	LDR R3, addr_value3
-	LDR R4, addr_value4
-	LDR R5, addr_value5
-
-	VLDR S16, [R1]		@ load values into
-	VLDR S18, [R1]		@ registers
-	VLDR S20, [R1]
-	VLDR S22, [R1]
-	VLDR S24, [R2]
-	VLDR S26, [R3]
-	VLDR S28, [R4]
-	VLDR S30, [R5]
-
+/*	SUB SP, SP, #24	@ room for printf */
+	LDR R8,=value1	@ Get addr of values
+	LDR R9,=value2
+	LDR R6,=value3
+	MOV R10,#3
+ciclo:
+	VLDR S16, [R8]		@ load values into
+	VLDR S18, [R8,#4]		@ registers
+	VLDR S20, [R8,#8]
+	VLDR S22, [R8,#12]
+	VLDR S24, [R9]
+	VLDR S26, [R9,#4]
+	VLDR S28, [R9,#8]
+	VLDR S30, [R9,#12]
 lenstride:
-/* Set LEN=4 0b101 and STRIDE=2 0b11 */
+/* Set LEN(16-18)=4 0b011 and STRIDE(20-21)=2 0b11 */
 	VMRS R3, FPSCR		@ get current FPSCR
-	MOV R4,  #0b11011	@ bit pattern for QEMU 
+	MOV R4,  #0b11011	@ bit pattern
 	MOV R4, R4, LSL #16	@ move across to b21
 	ORR R3, R3, R4		@ keep all 1's
-	VMSR FPSCR, R3		@ transfer to FPSCR
+	VMSR FPSCR, R3		@ transfer to FPSCR 
+	VADD.F32 S8, S16, S24	@ Vector addition in parallel
 
-	VADD.F32 S8, S16, S24	@ Vector addition
-	
+	VSTR S8, [R6]
+	VSTR S10, [R6,#4]
+	VSTR S12, [R6,#8]
+	VSTR S14, [R6,#12]
 
-convert:
+	ADD R6,#16
+	ADD R8,#16
+	ADD R9,#16
+	SUBS R10,#1
+	BNE  ciclo
+
+convert_and_print:
 /* Do conversion for printing, making sure not */
-/* to corrupt Sx registers by over writing     */
-
-	VCVT.F64.F32 D0, S10	
-	VCVT.F64.F32 D1, S12	
-	VCVT.F64.F32 D2, S14	
-	VCVT.F64.F32 D3, S8
-	LDR R0, =string		@ set up for printf
+/* to corrupt Sx registers by over writing */
+	MOV R10,#12
+	LDR R6,add_value3
+imprimir:
+	VLDR S8,[R6]
+	VCVT.F64.F32 D0, S8
+	LDR R0,=formatoF		@ set up for printf
 	VMOV R2, R3, D0
-	VSTR D1, [SP] 		@ push data on stack
-	VSTR D2, [SP, #8]
-	VSTR D3, [SP, #16]
 	BL printf
-	ADD SP, SP, #24		@ restore stack
-
-_exit:	
+	ADD R6,#4
+	SUBS R10,#1
+	BNE imprimir 
+_exit:
 	MOV R0, #0
 	MOV R7, #1
 	SWI 0
 
-addr_value1:	.word value1
-addr_value2:	.word value2
-addr_value3:	.word value3
-addr_value4:	.word value4
-addr_value5:	.word value5
+add_value3: .word value3
 
 	.data
-value1:	.float 1.0
-value2:	.float 1.25
-value3:	.float 1.50
-value4: 	.float 1.75
-value5:	.float 2.0
+value1:	.float 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0
+value2:	.float 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.101, 0.11, 0.12
+value3:	.float 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
 
-string:	
-.asciz " S10 is %f\n S12 is %f\n S14 is %f\n S8 is %f\n"
+string:
+.asciz " S8 is %f\n S10 is %f\n S12 is %f\n S14 is %f\n"
+
+formatoF:
+.asciz  "Valor %f\n"
+
